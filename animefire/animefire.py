25bd1a5d4ac873abd35ef1eada83b5f856e6dcae
Leandro de Jesus Santos <jstleandro@gmail.com>
2023-03-04 22:18:03 -0300

    site aborted

--- animefire/animefire.py
+++ animefire/animefire.py
@@ -4,4 +4,10 @@ from bs4 import BeautifulSoup
 
 class AnimeFire:
     def __init__(self):
-        self.__site_url = 'https://animefire.net/'
+        self.__site_url = ''
+        self.__animes_in_release = []
+        self.__episodes_in_release = []
+
+
+if __name__ == '__main__':
+    ...
